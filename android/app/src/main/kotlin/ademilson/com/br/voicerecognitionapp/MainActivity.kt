5724789593ea70f999496430ae9396432ffcdfe8
Ademilson Santana <ademilsonssilva1@gmail.com>
2020-08-22 01:47:47 -0300

    Primeiro commit| Falta melhorar alguns comportamentos

---
+++ android/app/src/main/kotlin/ademilson/com/br/voicerecognitionapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package ademilson.com.br.voicerecognitionapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:voicerecognitionapp/screens/home_screen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Voice Recognition App',
+    home: HomeScreen()
+  ));
+}
